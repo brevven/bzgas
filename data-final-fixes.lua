5e113a9cda5a591dffcebabe30aaf0c44edf8b97
Brevven <brevingame@gmail.com>
2022-07-17 05:08:34 -0700

    green circuits

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,23 +1,16 @@
 require("tin-recipe-final-stacking")
 require("tin-recipe-modules")
 -- require("tin-recipe-final-5d")
-require("tin-recipe-final-rrr")
 
 local util = require("data-util");
-util.replace_some_ingredient("se-glass-vulcanite", "sand", 1, "tin-plate", 1)
-util.add_product("se-glass-vulcanite", {type="item", name="tin-plate", amount=1, probability=0.8})
-util.set_main_product("se-glass-vulcanite", "glass")
 
--- core mining balancing
-util.add_to_product("se-core-fragment-omni", "tin-ore", -4)
+-- core mining balancing TODO
 
 
 -- Electronic circuits need final fixes
-util.multiply_recipe("electronic-circuit-stone", 2)
-util.replace_some_ingredient("electronic-circuit-stone", "copper-cable", 1, "solder", 1)
-
-util.multiply_recipe("electronic-circuit", 2)
-util.replace_some_ingredient("electronic-circuit", "copper-cable", 1, "solder", 1)
+util.replace_ingredient("electronic-circuit-stone", "stone-tablet", "bakelite")
+util.replace_ingredient("electronic-circuit", "wood", "bakelite")
+util.replace_ingredient("electronic-circuit", "iron-plate", "bakelite")
 
 -- Must be last
 util.create_list()
