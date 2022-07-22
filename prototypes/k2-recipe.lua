6138619d33d9e8b83686981a7dadaf220560a132
Brevven <brevingame@gmail.com>
2022-07-21 23:54:48 -0700

    k2 formaldehyde recipe

--- prototypes/k2-recipe.lua
+++ prototypes/k2-recipe.lua
@@ -20,6 +20,27 @@ if mods.Krastorio2 then
         {type="fluid", name="hydrogen", amount = 100},
       },
     },
+    {
+      type = "recipe",
+      name = "formaldehyde-methanol",
+      category = "chemistry",
+      main_product = "formaldehyde",
+      icons = {
+        {icon =  "__bzgas__/graphics/icons/formaldehyde.png", icon_size = 128, scale = 0.125},
+        {icon = kr_fluids_icons_path.."oxygen.png", icon_size = 64, scale = 0.125, icon_mipmaps = 4, shift={-8,-8}},
+      },
+      enabled = "false",
+      ingredients = {
+        {type="fluid", name="biomethanol", amount=50},
+        {type="fluid", name="oxygen", amount=25},
+      },
+      energy_required = 7,
+      results = {
+        {type="fluid", name="formaldehyde", amount = 50},
+        {type="fluid", name="water", amount = 50},
+      },
+    },
   })
   util.add_effect("kr-advanced-chemistry", {type="unlock-recipe", recipe="gas-reforming"})
+  util.add_effect("kr-advanced-chemistry", {type="unlock-recipe", recipe="formaldehyde-methanol"})
 end
