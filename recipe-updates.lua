147acde38d44429f05bef450704cea0f9215617e
Brevven <brevingame@gmail.com>
2022-07-30 23:42:32 -0700

    nits

--- recipe-updates.lua
+++ recipe-updates.lua
@@ -14,13 +14,11 @@ util.replace_ingredient("assembling-machine-2", "iron-gear-wheel", "bakelite")
 util.replace_some_ingredient("explosives", "coal", 1, "formaldehyde", 10)
 
 
-
 -- Gas boiler updates, from this mod or another
 util.remove_ingredient("gas-boiler", "pump")
 util.add_ingredient("gas-boiler", "pipe", 1)
 util.add_ingredient("gas-boiler", "solder", 1)
 util.add_effect("gas-extraction", {type="unlock-recipe", recipe="gas-boiler"})
-
 
 
 if mods.Krastorio2 then
@@ -33,7 +31,6 @@ util.replace_ingredient("se-bio-combustion-data", "se-plasma-stream", "gas")
 util.replace_ingredient("se-bio-combustion-resistance-data", "se-plasma-stream", "gas")
 util.add_ingredient("se-genetic-data", "phenol", 1)
 util.add_ingredient("se-comparative-genetic-data", "phenol", 2)
-
 
 
 -- Bob's Electronics SE KR
